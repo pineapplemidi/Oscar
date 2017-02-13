b454c43dd7313bcc30e158a7905daca872af755f
Matthew Berryhill <Matthew Berryhill>
2017-02-12 17:04:34 -0700

    refactored to use switch statement

--- midi_interface.c
+++ midi_interface.c
@@ -12,9 +12,15 @@ static uint8_t ReceivedDataBuffer[64];
 
 void handleMidi() {
     if(!USBHandleBusy(USBRxHandle)) {
-        if (ReceivedDataBuffer[1]==0x90) {//note on
-            if (ReceivedDataBuffer[2]==0x00) LATEbits.LATE1 = 0; // E1 = led output
-            if (ReceivedDataBuffer[2]==0x01) LATEbits.LATE1 = 1; //note C#
+//        if (ReceivedDataBuffer[1]==0x90) {//note on
+//            if (ReceivedDataBuffer[2]==0x00) LATEbits.LATE1 = 0; // E1; led off
+//            if (ReceivedDataBuffer[2]==0x01) LATEbits.LATE1 = 1; // E; led on
+        
+            
+        switch (ReceivedDataBuffer[1]) {
+            case 0x90: // note on
+                if (ReceivedDataBuffer[2]==0x00) LATEbits.LATE1 = 0; // E1; led off
+                if (ReceivedDataBuffer[2]==0x01) LATEbits.LATE1 = 1; // E; led on
         }
         
         USBRxHandle = USBRxOnePacket(USB_DEVICE_AUDIO_MIDI_ENDPOINT,(uint8_t*)&ReceivedDataBuffer,64); //Get ready for next packet (this will overwrite the old data)
