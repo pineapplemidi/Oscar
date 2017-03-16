551bec60572f6a282face474fa681276bd4ee71e
matt <matthewberryhill@gmail.com>
2017-03-15 21:12:27 -0600

    Implemented sysex into midi_interface

--- midi_interface.c
+++ midi_interface.c
@@ -1,7 +1,10 @@
 #include "usb.h"
 #include "usb_device_midi.h"
+#include "midi_interface.h"
 
 unsigned long midiInData;
+char messageLength;
+char k;
 static USB_HANDLE USBTxHandle;
 static USB_HANDLE USBRxHandle;
 static USB_AUDIO_MIDI_EVENT_PACKET midiData;
@@ -11,12 +14,21 @@ static uint8_t ReceivedDataBuffer[64];
 void handleMidi() {
     if(!USBHandleBusy(USBRxHandle)) {
         switch (ReceivedDataBuffer[1]) {
-            case 0x90: // note on
+            case 0x00: // handle sysex
+                handleSysex();
+            case 0x90: // handle note on
                 if (ReceivedDataBuffer[2]==0x00) LATEbits.LATE1 = 0; // E1; led off
                 if (ReceivedDataBuffer[2]==0x01) LATEbits.LATE1 = 1; // E; led on
         }
         
         USBRxHandle = USBRxOnePacket(USB_DEVICE_AUDIO_MIDI_ENDPOINT,(uint8_t*)&ReceivedDataBuffer,64); //Get ready for next packet (this will overwrite the old data)
+    }
+}
+
+void handleSysex() {
+    messageLength = ReceivedDataBuffer[2]; // length of Sysex Message
+    for (k=2; k<messageLength+2; k++) {
+        // todo Handle Message
     }
 }
 
--- midi_interface.h
+++ midi_interface.h
@@ -4,6 +4,7 @@
 #define __MIDI_INTERFACE_
 
 void handleMidi(void);
+void handleSysex(void);
 void sendMidiNoteOn(char cc, char velocity, char channel);
 void sendMidiNoteOff(char cc, char velocity, char channel);
 void sendMidiControlChange(char cc, char velocity, char channel);
